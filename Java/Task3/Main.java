998f2c3e0c58622c1eb2e7561d774e07189f83ae
StrangeresAnn <lykova_annushka@mail.ru>
2023-03-30 15:12:05 +0400

    Changed Task3

--- Java/Task3/Main.java
+++ Java/Task3/Main.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import Task3.drags.Component;
 import Task3.drags.impl.components.Azitron;
+import Task3.drags.impl.components.Pinecilin;
 
 
 public class Main {
@@ -14,9 +15,9 @@ public class Main {
         
         Component Azitron1 = new Azitron("Азитрон","2",5);
         Component Azitron2 = new Azitron("Азитрон","2",5);
-        Component Pinecilin = new Azitron("Пинецилин","10",7);
-        Component Pinecilin2 = new Azitron("Пинецилин","15",7);
-        Component Pinecilin3 = new Azitron("Пинецилин","15",9);
+        Component Pinecilin = new Pinecilin("Пинецилин","10",7);
+        Component Pinecilin2 = new Pinecilin("Пинецилин","15",7);
+        Component Pinecilin3 = new Pinecilin("Пинецилин","15",9);
         Component Pinecilin4 = Pinecilin3;
         List<Component> components = new ArrayList<>();
         components.add(Azitron1);
